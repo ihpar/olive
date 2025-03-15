4c8d96f6b6eed49a7e9e81b4b362cc3cd2187455
ihpar <ismail.hakki.parlak@gmail.com>
2025-03-15 22:07:50 +0300

    adss new experiment data and code

---
+++ classes.py
@@ -0,0 +1,24 @@
+classes = ["Air",
+           "O 100", "O 95", "O 90", "O 80",
+           "O 70", "O 50", "O 0"]
+colors = ["#2c73d2",
+          "#327d17", "#508f21", "#6ca02c", "#88b237",
+          "#a4c444", "#c2d551", "#e0e75f"]
+AIR = 0
+OLIVE_100 = 1
+OLIVE_95 = 2
+OLIVE_90 = 3
+OLIVE_80 = 4
+OLIVE_70 = 5
+OLIVE_50 = 6
+OLIVE_0 = 7
+
+regression_targets = {
+    OLIVE_100: 100,
+    OLIVE_95: 95,
+    OLIVE_90: 90,
+    OLIVE_80: 80,
+    OLIVE_70: 70,
+    OLIVE_50: 50,
+    OLIVE_0: 0
+}
